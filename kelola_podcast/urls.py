b91ba350c40b06f58e24a160d8587b52b31daadc
ryandhika_al <ryandhika.al@ui.ac.id>
2024-05-19 13:50:41 +0700

    CRUD Podcast almost done, only create episode that isn't working yet

--- kelola_podcast/urls.py
+++ kelola_podcast/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path('podcasts/', views.podcast_management, name='podcast_management'),
-    path('podcasts/<int:podcast_id>/', views.view_podcast, name='view_podcast'),
-    path('podcasts/<int:podcast_id>/delete/', views.delete_podcast, name='delete_podcast'),
-    path('podcasts/<int:podcast_id>/episodes/', views.view_episode, name='view_episode'),
-    path('podcasts/<int:podcast_id>/episodes/add/', views.add_episode, name='add_episode'),
-    path('episodes/<int:episode_id>/delete/', views.delete_episode, name='delete_episode'),
+    path('podcastEpisode/<uuid:podcast_id>/', views.podcast_episode_management, name='podcast_episode_management'),
+    path('deletePodcasts/<uuid:podcast_id>/', views.delete_podcast, name='delete_podcast'),
+    path('deleteEpisodes/<uuid:episode_id>/', views.delete_episode, name='delete_episode'),
 ]
--- kelola_podcast/views.py
+++ kelola_podcast/views.py
@@ -1,41 +1,98 @@
 from django.shortcuts import render, redirect
 from utils.db import data_from_db
+from django.http import Http404
+import uuid
+from django.utils import timezone
+
+#Untuk simpan detail podcast walaupun hanya dipakai judulnya saja
+def get_podcast_details(podcast_id):
+    con = data_from_db()
+    cur = con.cursor()
+
+    # Query untuk mendapatkan detail podcast berdasarkan ID
+    cur.execute("""
+        SELECT K.id, K.judul, array_agg(G.genre), A.nama, K.durasi, K.tanggal_rilis, K.tahun
+        FROM KONTEN K
+        JOIN PODCAST P ON K.id = P.id_konten
+        JOIN PODCASTER Po ON P.email_podcaster = Po.email
+        JOIN AKUN A ON Po.email = A.email
+        JOIN GENRE G ON K.id = G.id_konten
+        WHERE K.id = %s
+        GROUP BY K.id, K.judul, A.nama, K.durasi, K.tanggal_rilis, K.tahun
+    """, (podcast_id,))
+    podcast_details = cur.fetchone()
+
+    cur.close()
+    con.close()
+
+    return podcast_details
+
+def get_episode_list(podcast_id):
+    con = data_from_db()
+    cur = con.cursor()
+
+    # Query untuk mendapatkan daftar episode dari suatu podcast
+    cur.execute("""
+        SELECT id_episode, judul, deskripsi, durasi, tanggal_rilis
+        FROM EPISODE
+        WHERE id_konten_podcast = %s
+        ORDER BY tanggal_rilis DESC
+    """, (podcast_id,))
+    episode_list = cur.fetchall()
+
+    cur.close()
+    con.close()
+
+    return episode_list
+
+from main.views import get_user_roles
 
 def podcast_management(request):
     con = data_from_db()
     cur = con.cursor()
 
     if request.method == 'POST':
-        # Logika untuk membuat podcast baru
-        title = request.POST.get('title')
-        genre = request.POST.getlist('genre')
+        # Mendapatkan alamat email podcaster
+        email = request.session.get('email')
         
-        # Mendapatkan UUID baru
-        cur.execute("SELECT uuid_generate_v4()")
-        new_uuid = cur.fetchone()[0]  # Mengambil UUID dari hasil query
+        # Mendapatkan peran pengguna yang saat ini login
+        roles = get_user_roles(email)
         
-        # Memasukkan data ke dalam database
-        cur.execute("""
-            INSERT INTO KONTEN (id, judul, tanggal_rilis, tahun, durasi)
-            VALUES (%s, %s, CURRENT_DATE, EXTRACT(YEAR FROM CURRENT_DATE), 0)
-        """, [new_uuid, title])
-        
-        cur.execute("""
-            INSERT INTO PODCAST (id_konten, email_podcaster)
-            VALUES (%s, %s)
-        """, [new_uuid, 'podcaster@example.com'])
+        # Hanya menjalankan logika jika pengguna memiliki peran podcaster
+        if 'Podcaster' in roles:
+            # Logika untuk membuat podcast baru
+            title = request.POST.get('title')
+            genre = request.POST.getlist('genre')
 
-        for g in genre:
+            # Generate random UUID
+            random_id = uuid.uuid4()
+            current_time = timezone.now()
+
+            duration = 0
+
+            # Execute SQL command with random ID
             cur.execute("""
-                INSERT INTO GENRE (id_konten, genre)
-                VALUES (%s, %s)
-            """, [new_uuid, g])
+                INSERT INTO KONTEN (id, judul, tanggal_rilis, tahun, durasi)
+                VALUES (%s, %s, %s,%s, %s)
+            """, [random_id, title, current_time, current_time.year, 0])
 
-        con.commit()
+            for g in genre:
+                    cur.execute("""
+                        INSERT INTO GENRE (id_konten, genre)
+                        VALUES (%s, %s)
+                    """, [random_id, g])
+
+            # Menyimpan podcast dengan alamat email podcaster
+            cur.execute("""
+                INSERT INTO PODCAST (id_konten, email_podcaster)
+                VALUES(%s, %s)
+            """, [random_id, email])
+
+            con.commit()
     
     # Logika untuk menampilkan daftar podcast
     cur.execute("""
-        SELECT K.judul, COUNT(E.id_episode) AS jumlah_episode, COALESCE(SUM(E.durasi), 0) AS total_durasi
+        SELECT K.id, K.judul, COUNT(E.id_episode) AS jumlah_episode, COALESCE(SUM(E.durasi), 0) AS total_durasi
         FROM KONTEN K
         JOIN PODCAST P ON K.id = P.id_konten
         LEFT JOIN EPISODE E ON K.id = E.id_konten_podcast
@@ -46,9 +103,10 @@ def podcast_management(request):
 
     podcast_list = [
         {
-            'judul': row[0],
-            'jumlah_episode': row[1],
-            'total_durasi': row[2]
+            'id': row[0],              
+            'judul': row[1],
+            'jumlah_episode': row[2],
+            'total_durasi': row[3]
         }
         for row in podcast_list
     ]
@@ -58,89 +116,72 @@ def podcast_management(request):
 
     return render(request, 'PodcastManagement.html', {'podcast_list': podcast_list})
 
-def podcast_detail(request, podcast_id):
+def podcast_episode_management(request, podcast_id):
+    podcast_details = get_podcast_details(podcast_id)
     con = data_from_db()
     cur = con.cursor()
+    random_id = uuid.uuid4()
+    current_time = timezone.now()
 
-    # Fetch podcast details based on the given podcast_id
-    cur.execute("""
-        SELECT K.judul, STRING_AGG(DISTINCT G.genre, ', ') AS genre, A.nama, K.tanggal_rilis, K.tahun
-        FROM PODCAST P
-        JOIN KONTEN K ON P.id_konten = K.id
-        JOIN GENRE G ON K.id = G.id_konten
-        JOIN PODCASTER PDR ON P.email_podcaster = PDR.email
-        JOIN AKUN A ON PDR.email = A.email
-        WHERE P.id_konten = %s
-        GROUP BY K.judul, A.nama, K.tanggal_rilis, K.tahun
-    """, [podcast_id])
-    
-    podcast_detail = cur.fetchone()
+    if request.method == 'POST':
+        # Ambil data dari form dan tambahkan episode baru
+        title = request.POST['title']
+        description = request.POST['description']
+        duration = request.POST['duration']
+        
+        cur.execute("""
+                INSERT INTO EPISODE (id_episode, id_konten_podcast, judul, deskripsi, durasi, tanggal_rilis)
+                VALUES (%s, %s, %s, %s, %s, %s)  
+        """, [random_id, podcast_id, title, description, duration, current_time])  
 
-    if not podcast_detail:
-        return render(request, '404.html', {'error': 'Podcast not found'})
+    episode_list = get_episode_list(podcast_id)
 
-    podcast_detail = {
-        'judul': podcast_detail[0],
-        'genre': podcast_detail[1],
-        'nama_podcaster': podcast_detail[2],
-        'tanggal_rilis': podcast_detail[3],
-        'tahun': podcast_detail[4]
+    podcast_data = {
+        'id': podcast_details[0],
+        'judul': podcast_details[1]
     }
+
+    episodes = [
+        {
+            'id': episode[0], 
+            'judul': episode[1],
+            'deskripsi': episode[2],
+            'durasi': format_duration(episode[3]),
+            'tanggal_rilis': episode[4]
+        } for episode in episode_list
+    ]
 
     cur.close()
     con.close()
 
-    return render(request, 'PodcastEpisodeManagement.html', {'podcast_detail': podcast_detail})
-
-def view_podcast(request, podcast_id):
-    con = data_from_db()
-    cur = con.cursor()
-    cur.execute("SELECT * FROM Podcast WHERE id = %s", (podcast_id,))
-    podcast = cur.fetchone()
-    # Fetch episodes for the podcast
-    cur.execute("SELECT * FROM Episode WHERE podcast_id = %s", (podcast_id,))
-    episodes = cur.fetchall()
-    return render(request, 'PodcastEpisodeManagement.html', {'podcast': podcast, 'episodes': episodes})
+    return render(request, 'PodcastEpisodeManagement.html', {'episodes': episodes, 'podcast_data': podcast_data})
 
 def delete_podcast(request, podcast_id):
     if request.method == 'POST':
         con = data_from_db()
         cur = con.cursor()
-        cur.execute("DELETE FROM Podcast WHERE id = %s", (podcast_id,))
+        cur.execute("""
+                DELETE FROM KONTEN
+                WHERE id = %s
+            """, [podcast_id])
         con.commit()
         return redirect('podcast_management')
     
-def view_episode(request, podcast_id):
-    con = data_from_db()
-    cur = con.cursor()
-    cur.execute("SELECT * FROM Episode WHERE podcast_id = %s", (podcast_id,))
-    episodes = cur.fetchall()
-    
-    context = {
-        'episodes': episodes
-    }
-    return render(request, 'PodcastEpisodeManagement.html', context)
+def format_duration(duration):
+    hours = duration // 60
+    minutes = duration % 60
+    return f"{hours} jam {minutes} menit" if hours > 0 else f"{minutes} menit"
 
-def add_episode(request, podcast_id):
-    if request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
-        duration = request.POST['duration']
-        
-        con = data_from_db()
-        cur = con.cursor()
-        cur.execute("INSERT INTO Episode (title, description, duration, podcast_id) VALUES (%s, %s, %s, %s)",
-                    (title, description, duration, podcast_id))
-        con.commit()
-        
-        return redirect('view_episode', podcast_id=podcast_id)
-    else:
-        return render(request, 'PodcastEpisodeManagement.html')
-    
 def delete_episode(request, episode_id):
     con = data_from_db()
     cur = con.cursor()
-    cur.execute("DELETE FROM Episode WHERE id = %s", (episode_id,))
+    cur.execute("""
+                DELETE FROM EPISODE
+                WHERE id_episode = %s
+            """, [episode_id])
     con.commit()
+    cur.close()
+    con.close()
     return redirect(request.META.get('HTTP_REFERER', 'podcast_episode_management'))
+
 
