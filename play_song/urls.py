5f5daac10aeec94a1836265aa9c94305a4a56886
Joseph BIntang <joseph.bintang@ui.ac.id>
2024-05-19 16:48:32 +0700

    tambahan backend

--- play_song/urls.py
+++ play_song/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from play_song.views import *
+from .views import play_song, add_song_to_user_playlist, download_song
+
+app_name = 'play_song'
 
 urlpatterns = [
-    path('play-song/', play_song, name='play_song'),
+    path('coba/<str:id_konten>/', play_song, name="play_song"),
+    path('add_song_to_user_playlist/<str:id_konten>/', add_song_to_user_playlist, name='add_song_to_user_playlist'),
+    path('download_song/<str:id_konten>/', download_song, name="download_song"),
+    
 ]
