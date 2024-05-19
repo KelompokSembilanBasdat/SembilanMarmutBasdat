8dcdd8677dea7db35a52cd6fe50c74a4d961060e
almayawn <alma.laras@ui.ac.id>
2024-05-19 11:31:03 +0700

    Implemented cek royalti and kelola album dan song

--- cek_royalti/views.py
+++ cek_royalti/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def cek_royalti(request):
-    
     email = request.session.get('email')
     roles = request.session.get('roles')
     if email is None:
@@ -23,33 +22,26 @@ def cek_royalti(request):
                 JOIN LABEL ON ALBUM.id_label = LABEL.id
                 JOIN ROYALTI ON SONG.id_konten = ROYALTI.id_song
                 WHERE LABEL.email = '{email}'
-                AND ROYALTI.id_pemilik_hak_cipta = (SELECT id_pemilik_hak_cipta FROM LABEL WHERE email = '{email}');
+                AND ROYALTI.id_pemilik_hak_cipta = LABEL.id_pemilik_hak_cipta
+                ORDER BY total_royalties DESC;
         """
     else:
          cur.execute("SELECT update_royalties(%s)", (email,))
          query = f"""
-                SELECT KONTEN.judul, ALBUM.judul, SONG.total_play, SONG.total_download, ROYALTI.jumlah
-                FROM SONG
-                JOIN KONTEN ON SONG.id_konten = KONTEN.id
-                JOIN ALBUM ON SONG.id_album = ALBUM.id
-                JOIN ROYALTI ON SONG.id_konten = ROYALTI.id_song
-                WHERE SONG.id_artist = (SELECT id FROM ARTIST WHERE email_akun = '{email}')
-                AND ROYALTI.id_pemilik_hak_cipta = (SELECT id_pemilik_hak_cipta FROM ARTIST WHERE email_akun = '{email}')
-
-                UNION
-
-                SELECT KONTEN.judul, ALBUM.judul, SONG.total_play, SONG.total_download, ROYALTI.jumlah
-                FROM SONG
-                JOIN KONTEN ON SONG.id_konten = KONTEN.id
-                JOIN ALBUM ON SONG.id_album = ALBUM.id
-                JOIN SONGWRITER_WRITE_SONG ON SONG.id_konten = SONGWRITER_WRITE_SONG.id_song
-                JOIN ROYALTI ON SONG.id_konten = ROYALTI.id_song
-                WHERE SONGWRITER_WRITE_SONG.id_songwriter = (SELECT id FROM SONGWRITER WHERE email_akun = '{email}')
-                AND ROYALTI.id_pemilik_hak_cipta = (SELECT id_pemilik_hak_cipta FROM SONGWRITER WHERE email_akun = '{email}');
+            SELECT KONTEN.judul, ALBUM.judul, SONG.total_play, SONG.total_download, SUM(ROYALTI.jumlah) AS total_royalties
+            FROM SONG
+            JOIN KONTEN ON SONG.id_konten = KONTEN.id
+            JOIN ALBUM ON SONG.id_album = ALBUM.id
+            JOIN ROYALTI ON SONG.id_konten = ROYALTI.id_song
+            LEFT JOIN SONGWRITER_WRITE_SONG ON SONG.id_konten = SONGWRITER_WRITE_SONG.id_song
+            WHERE (SONG.id_artist = (SELECT id FROM ARTIST WHERE email_akun = '{email}') OR
+                SONGWRITER_WRITE_SONG.id_songwriter = (SELECT id FROM SONGWRITER WHERE email_akun = '{email}'))
+            GROUP BY KONTEN.judul, ALBUM.judul, SONG.total_play, SONG.total_download
+            ORDER BY total_royalties DESC;
         """
 
     cur.execute(query)
     royalties = cur.fetchall()
     cur.close()
     conn.close()
-    return render(request, 'cek_royalti.html', {'royalties': royalties})
+    return render(request, 'cek_royalti.html', {'request': request, 'royalties': royalties})
--- kelola_album_dan_song/urls.py
+++ kelola_album_dan_song/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from kelola_album_dan_song.views import *
 
 urlpatterns = [
-    path('artist/', kelola_album_song_artis, name='kelola_album_song_artis'),
-    path('add_song/', add_song, name='add_song'),
+    path('', kelola_album_song, name='kelola_album_song'),
     path('add_album/', add_album, name='add_album'),
-    path('view_album/', view_album, name='view_album'),
+    path('add_album_submit/', add_album_submit, name='add_album_submit'),
+    path('add_song/<uuid:id_album>/', add_song, name='add_song'),
+    path('add_song_submit/<uuid:id_album>/', add_song_submit, name='add_song_submit'),
+    path('view_album/<uuid:id_album>/', view_album, name='view_album'),
+    path('delete_album/<uuid:id_album>/', delete_album, name='delete_album'),
+    path('delete_song/<uuid:id_album>/<uuid:id_song>', delete_song, name='delete_song')
 ]
